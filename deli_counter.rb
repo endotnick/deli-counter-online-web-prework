95bf7e5a6e098d8af913d528f510726d54738f5b
Nick Rodriguez <nickbenrodriguez@gmail.com>
2018-11-20 02:38:54 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,4 +20,9 @@ def take_a_number(deli, name)
 end
 
 def now_serving(deli)
+  if deli.size == 0
+    puts 'There is nobody waiting to be served!'
+  else
+    puts "Now serving #{deli.pop}."
+  end
 end
