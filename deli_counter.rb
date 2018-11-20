c9a1d71dd503b8c3190b59b711383c9f8c6bc77f
Nick Rodriguez <nickbenrodriguez@gmail.com>
2018-11-20 02:35:50 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,14 @@
 def line(deli)
   puts 'The line is currently empty.' if deli.size == 0
+  display_line(deli)
 end
 
 def display_line(line)
-
+  out = 'The line is currently:'
+  line.each_with_index do |customer, index|
+    out += " #{index}. #{customer}"
+  end
+  puts out
 end
 
 def take_a_number(deli, name)
